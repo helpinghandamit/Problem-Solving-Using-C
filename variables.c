fa62492541eaa208aa2a2189e633cfa891cbb4d1
helpinghandamit <151981635+helpinghandamit@users.noreply.github.com>
2023-12-09 17:41:37 +0530

    Create variables.c
    Program to accept the values of two variables:

---
+++ variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    int var1, var2;
+    printf("Enter two variables: ");
+    scanf("%d %d", &var1, &var2);
+    printf("You entered: %d and %d\n", var1, var2);
+    getch();
+}
