51d4e9785ba29a1ceab7b6df9c4f11c10accb603
helpinghandamit <151981635+helpinghandamit@users.noreply.github.com>
2023-12-26 14:38:24 +0530

    Create unformatted_output_Functions.c
    PROGRAM to find area of rectangle and print the result using unformatted output Functions

---
+++ unformatted_output_Functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    float length, width, area;
+    
+    printf("Enter length and width of the rectangle: ");
+    scanf("%f %f", &length, &width);
+    
+    area = length * width;
+    
+    printf("Area of the rectangle: %.2f\n", area);
+    
+    getch();
+}
