10ebd7d9043680c26213399c244656859a0965d5
helpinghandamit <151981635+helpinghandamit@users.noreply.github.com>
2023-12-26 14:36:51 +0530

    Create unformatted_Input_Functions.c
    PROGRAM using unformatted Input Functions

---
+++ unformatted_Input_Functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    int num;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    
+    printf("You entered: %d\n", num);
+    
+    getch();
+}
