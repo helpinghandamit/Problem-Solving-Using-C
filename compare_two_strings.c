f589c421d19141e35bb9d7f74213dcc897e43cf8
helpinghandamit <151981635+helpinghandamit@users.noreply.github.com>
2024-01-20 17:54:48 +0530

    Create compare_two_strings.c
    A PROGRAM to compare a string

---
+++ compare_two_strings.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+    char str1[100], str2[100];
+    
+    printf("Enter the first string: ");
+    scanf("%s", str1);
+    
+    printf("Enter the second string: ");
+    scanf("%s", str2);
+    
+    if (strcmp(str1, str2) == 0)
+        printf("Strings are equal.\n");
+    else
+        printf("Strings are not equal.\n");
+    
+    getch();
+}
